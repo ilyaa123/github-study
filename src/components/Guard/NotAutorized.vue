8e6602001862bd01d3d68e899964e91fd5f73031
ilyaa123 <ilya.a2145@gmail.com>
2023-10-18 20:45:00 +0300

    add not autorized page

--- src/components/Guard/NotAutorized.vue
+++ src/components/Guard/NotAutorized.vue
@@ -1,8 +1,7 @@
 <template>
-	<div>
-		<h1>
-			<el-text class="mb-6" type="primary">Your not autorized!</el-text>
-		</h1>
-		<el-link href="/login" type="primary" :underline="false">Login</el-link>
-	</div>
+	<el-empty description="Your are not autorized!">
+		<el-link href="/login" type="primary" :underline="false"
+			><h3>Login</h3></el-link
+		>
+	</el-empty>
 </template>
